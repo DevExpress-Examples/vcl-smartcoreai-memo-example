3e0ccd56a56360db83da781ae4a5f2ed4849f2fe
Aleksandr Krivopalov <aleksandr.krivopalov@devexpress.com>
2025-11-21 09:43:51 +0200

    Improved error handling in TdxSmartCoreAIChatClient

--- CS/dxSmartCoreAI.pas
+++ CS/dxSmartCoreAI.pas
@@ -17,11 +17,14 @@ type
     FRequest: TAIChatRequest;
     FResponseEvent: TEvent;
     FLastResponse: string;
+    FLastError: string;
     function ConvertResponse(const AResponseText: string): TdxAIChatResponse;
+    function ConvertError(const AErrorMessage: string): TdxAIChatResponse;
     function GetCombinedPrompt(const AMessages: IEnumerable<TdxAIChatMessage>): string;
     function PerformChatRequest(const AMessages: IEnumerable<TdxAIChatMessage>;
       const AOptions: TdxAIChatOptions): TdxAIChatResponse;
     procedure ResponseHandler(ASender: TObject; const AText: string);
+    procedure ErrorHandler(ASender: TObject; const AErrorMessage: string);
   public
     constructor Create(ADriver: TAIDriver);
     destructor Destroy; override;
@@ -40,6 +43,7 @@ begin
   FRequest := TAIChatRequest.Create(nil);
   FResponseEvent := TEvent.Create;
   FRequest.OnResponse := ResponseHandler;
+  FRequest.OnError := ErrorHandler;
   FConnection.Driver := ADriver;
   FRequest.Connection := FConnection;
 end;
@@ -70,6 +74,14 @@ begin
   Result := TdxAIChatResponse.Create(AMessage, TdxAIChatFinishReason.Stop);
 end;
 
+function TdxSmartCoreAIChatClient.ConvertError(const AErrorMessage: string): TdxAIChatResponse;
+var
+  AMessage: TdxAIChatMessage;
+begin
+  AMessage := TdxAIChatMessage.Create(TdxAIChatRole.Assistant, AErrorMessage);
+  Result := TdxAIChatResponse.Create(AMessage, TdxAIChatFinishReason.Error);
+end;
+
 function TdxSmartCoreAIChatClient.GetCombinedPrompt(const AMessages: IEnumerable<TdxAIChatMessage>): string;
 var
   AMessage: TdxAIChatMessage;
@@ -92,10 +104,17 @@ var
   ACombinedPrompt: string;
 begin
   FResponseEvent.ResetEvent;
+  FLastError := '';
+  FLastResponse := '';
   ACombinedPrompt := GetCombinedPrompt(AMessages);
   FRequest.Chat(ACombinedPrompt);
   if FResponseEvent.WaitFor(60_000) <> wrTimeout then
-    Result := ConvertResponse(FLastResponse)
+  begin
+    if FLastError <> '' then
+      Result := ConvertError(FLastError)
+    else
+      Result := ConvertResponse(FLastResponse)
+  end
   else
     raise Exception.Create('Timeout for a response from the model has been reached.');
 end;
@@ -103,6 +122,12 @@ end;
 procedure TdxSmartCoreAIChatClient.ResponseHandler(ASender: TObject; const AText: string);
 begin
   FLastResponse := AText;
+  FResponseEvent.SetEvent;
+end;
+
+procedure TdxSmartCoreAIChatClient.ErrorHandler(ASender: TObject; const AErrorMessage: string);
+begin
+  FLastError := AErrorMessage;
   FResponseEvent.SetEvent;
 end;
 
